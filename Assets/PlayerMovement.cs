45c6fb06ea6ae0f1319e09d086ab2fefb7761a28
Popey0424 <roylouis0424@gmail.com>
2024-11-27 15:35:37 +0100

    CharacterController 1.0

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;     
+    [SerializeField] private float rotationSpeed;
+    public Player.Etat PlayerEtat;
+
+    private Vector3 inputDirection;
+
+
+
+    void Update()
+    {
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        inputDirection = new Vector3(horizontal, 0, vertical).normalized;
+        Movement();
+       
+    }
+    private void Movement()
+    {
+
+        if (inputDirection.magnitude >= 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+            float angle = Mathf.LerpAngle(transform.eulerAngles.y, targetAngle, rotationSpeed * Time.deltaTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
+}
+
+[System.Serializable]
+public class Player
+{
+    public enum Etat { PlayerX, PlayerY }
+}
---
+++ Assets/SwitchPlayer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SwitchPlayer : MonoBehaviour
+{
+    //public enum Etat { PlayerX, PlayerY }
+    [SerializeField] private KeyCode switchMode;
+    private PlayerMovement playerMovement;
+    private Player.Etat currentEtat; 
+    public GameObject playerY;
+    public GameObject playerX;
+  
+
+    private void Start()
+    {   
+        
+        playerMovement = GetComponent<PlayerMovement>();
+        currentEtat = Player.Etat.PlayerX;
+        playerY.SetActive(false);
+
+       
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(switchMode))
+        {
+            SwitchState();
+        }
+    }
+
+    private void SwitchState()
+    {
+        if (currentEtat == Player.Etat.PlayerX)
+        {
+            currentEtat = Player.Etat.PlayerY;
+            playerY.SetActive(true);
+            playerY.transform.position = playerX.transform.position;
+            playerX.SetActive(false);
+            Debug.Log("Ca marche");
+        }
+        else if (currentEtat == Player.Etat.PlayerY)
+        {
+            currentEtat = Player.Etat.PlayerX;
+            playerX.SetActive(true);
+            playerY.SetActive(false);
+            Debug.Log("Ca marche");
+        }
+    }
+
+}
+
+
+
